bd5702423aaafa7e165069f6afaf3a48b9850793
Ben <btorndorff@gmail.com>
2023-11-16 12:42:27 -0800

    asda

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// App.jsx
+import React, { useState, useEffect } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/Nav";
+import Home from "./screens/Home";
+import videoBackground from "./assets/ghibli.mov";
+import "./App.css";
+import PhotoVideo from "./screens/PhotoVideo";
+import Illustrations from "./screens/Illustrations";
+// import Pastry from './Pastry';
+// import Writing from './Writing';
+import About from "./screens/About";
 
 function App() {
+  const { pathname } = useLocation();
+  const initialTab = pathname == "/" ? "home" : pathname.replace("/", "");
+
+  const [selectedTab, setSelectedTab] = useState(initialTab);
+
+  const changeSelectedTab = (tab) => {
+    console.log("changed");
+    setSelectedTab(tab);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <video className="background" autoPlay loop muted>
+          <source src={videoBackground} type="video/mp4" />
+        </video>
+        <Navbar selectedTab={selectedTab} setSelectedTab={changeSelectedTab} />
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/photo-video" element={<PhotoVideo />} />
+        <Route path="/illustrations" element={<Illustrations />} />
+        {/* <Route path="/pastry" element={Pastry} /> */}
+        {/* <Route path="/writing" element={Writing} /> */}
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
   );
 }
 
