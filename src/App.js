e33d05089003f2205138eff915358c54c19d4e6f
Ben Orndorff <ben.orndorff@Ben-Orndorff-Mac-CVQHG6N14F.local>
2023-11-17 00:10:15 -0800

    dlksnakld

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import About from "./screens/About";
 function App() {
   const { pathname } = useLocation();
   let initialTab = "home";
-  if (pathname !== "/" || pathname !== "addi-website") {
+  if (pathname !== "/" || pathname !== "addi-website/") {
     initialTab = pathname.replace("/", "");
   }
 
